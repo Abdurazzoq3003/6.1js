53c491af3d47e58e8b9bf23774e5439d5fe8fc29
Abdurazzoq3003 <aabdurazzoq80@gmail.com>
2024-09-18 19:57:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// console.log("BMW");
+
+// function GM() {
+//   console.log("Matiz");
+// }
+
+// GM();
+
+let ism = prompt("Ismingizni kiriting...");
+function salom(name) {
+  console.log(name, "Xush kelibsiz");
+}
+salom(ism);
